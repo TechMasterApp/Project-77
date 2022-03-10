892b4d31a08c6d9f043fbfeab3521e1a49dd3566
TechMasterApp <84641823+TechMasterApp@users.noreply.github.com>
2022-03-10 21:14:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var firstName = "Minecraft"
+var lastName = "Master"
+
+var fullName = firstName.concat(lastName)
+
+console.log(fullName)
+
+var num1 = 8
+var num2 = 4
+
+var add =  num1+num2
+var sub =  num1-num2
+var mul =  num1*num2
+var div =  num1/num2
+
+console.log(add)
+console.log(sub)
+console.log(mul)
+console.log(div)
